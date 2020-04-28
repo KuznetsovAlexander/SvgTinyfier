59dbf31094313de94f1d8137153b127fc339709c
Alex Kuznetsov <KuznetsovAlexander@rambler.ru>
2020-04-28 13:08:38 +0400

    Support for different opening tags.
    Signed-off-by: Alex Kuznetsov <KuznetsovAlexander@rambler.ru>

--- SvgTinyfier.cpp
+++ SvgTinyfier.cpp
@@ -22,9 +22,21 @@ std::string SvgTinyfier::tinyfy(const std::string& svg)
 
 std::string SvgTinyfier::findStyleSection(const std::string& svg)
 {
-  size_t openTagPos = svg.find("<style>");
+  std::vector<std::string> openingTags = {
+    "<style>",
+    "<style type=\"text/css\">"
+  };
+  int size = 0;
+  size_t openTagPos = 0;
+  for (auto tag : openingTags) {
+    openTagPos = svg.find(tag);
+    if (openTagPos != std::string::npos) {
+      size = tag.size();
+      break;
+    }
+  }
   size_t closeTagPos = svg.find("</style>");
-  std::string styleSection = svg.substr(openTagPos + 7, closeTagPos - openTagPos - 7);
+  std::string styleSection = svg.substr(openTagPos + size, closeTagPos - openTagPos - 7);
   return styleSection;
 }
 
@@ -45,6 +57,8 @@ std::map<std::string, std::string> SvgTinyfier::createStylesMap(const std::strin
   std::vector<std::string> styles;
   for (int i = 0; i < stylesSeparatorPos.size() - 1; ++i) {
     std::string style = styleSection.substr(stylesSeparatorPos[i], stylesSeparatorPos[i + 1] - stylesSeparatorPos[i]);
+    size_t begin_pos = style.find_first_not_of(" \n\t");
+    style = style.substr(begin_pos);
     styles.push_back(style);
   }
 
